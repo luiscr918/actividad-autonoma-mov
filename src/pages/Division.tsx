7ac16a413364bc2559df1cdb8b817b851d670ef7
luiscr918 <lc9107040@gmail.com>
2025-02-06 15:01:00 -0500

    Deber Listo para subor solo checar la vald de campos incompletos

--- src/pages/Division.tsx
+++ src/pages/Division.tsx
@@ -16,50 +16,64 @@ interface DivForm {
 export const Division = () => {
   //hook useState para manejar el estado del formulario
   const [divForm, setDivForm] = useState<DivForm>({
-    number1: 1,
-    number2: 1
+    number1: NaN,
+    number2: NaN
   });
   //Funcion para la captura y  cambio de los campos del form
   const handleChange = (name: string, value: string): void => {
     //modificar la data del divForm
-    setDivForm({ ...divForm, [name]: value });
+    setDivForm({ ...divForm, [name]:Number(value)  });
   }
   //Funcion para Guardar los numeros dando click
   const [resultado, setResultado] = useState<number>(0)
   const handleDivision = () => {
-    //verificar si se hizo el cambio correcto de valores del form
-    console.log(divForm);
-    setResultado((divForm.number1) / (divForm.number2)) ;
+    if (isNaN(divForm.number1) || isNaN(divForm.number2)) {
+      Alert.alert('Error', 'Campos incompletos');
+      return;
+    } else if (divForm.number1 === 0 && divForm.number2 === 0) {
+      Alert.alert('Error', 'Indeterminacion');
+      return;
+    }
+    else if (divForm.number2 === 0) {
+      Alert.alert('Error', 'No existe división para 0');
+      return;
+    } else {
+      //verificar si se hizo el cambio correcto de valores del form
+      console.log(divForm);
+      setResultado((divForm.number1) / (divForm.number2));
+    }
+
 
   }
   //funcion para realizar la division y devolver
 
-  const {height}=useWindowDimensions();
+  const { height } = useWindowDimensions();
 
 
   return (
     <SafeAreaView>
-      <Text style={{...styles.title,
-        height:height*0.15
+      <Text style={{
+        ...styles.title,
+        height: height * 0.15
       }}>Calculadora De División</Text>
       <BodyComponent pantalla='screenDiv'>
-      <View style={styles.containerTextInput}>
-        <InputComponent
-          placehorderText='Ingrese el primer número'
-          keyboardType='decimal-pad'
-          handleChange={handleChange}
-          name='number1'
-        />
+        <View style={styles.containerTextInput}>
+          <InputComponent
+            placehorderText='Ingrese el primer número'
+            keyboardType='decimal-pad'
+            handleChange={handleChange}
+            name='number1'
+          />
 
-        <InputComponent
-          placehorderText='Ingrese el segundo número'
-          keyboardType='decimal-pad'
-          handleChange={handleChange}
-          name='number2'
-        />
-      </View>
-      <ButtonComponent title='Dividir' handleDivision={handleDivision} />
-      <Text style={styles.textResultado} >El resultado de su division es: <Text style={styles.resutlado} >{resultado.toFixed(2)}</Text> </Text> 
+          <InputComponent
+            placehorderText='Ingrese el segundo número'
+            keyboardType='decimal-pad'
+            handleChange={handleChange}
+            name='number2'
+          />
+        </View>
+        <ButtonComponent title='Dividir' handleDivision={handleDivision} />
+        <Text style={styles.textResultado} >El resultado de su division es: <Text style={styles.resutlado} >{resultado.toFixed(2)}</Text> </Text>
       </BodyComponent>
     </SafeAreaView>
   )
